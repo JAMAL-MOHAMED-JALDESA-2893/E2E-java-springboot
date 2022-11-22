fc774ee1f7306313718d5962006b58db73b615ca
Jamal Hassan <jayhamo22@gmail.com>
2022-11-22 14:36:15 +0300

    add models

--- src/main/java/com/jamal/student/dal/entities/Student.java
+++ src/main/java/com/jamal/student/dal/entities/Student.java
@@ -1,5 +1,35 @@
 package com.jamal.student.dal.entities;
 
 public class Student {
+	
+	private Long id;
+	private String name;
+	private String course;
+	private Double fee;
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public Double getFee() {
+		return fee;
+	}
+	public void setFee(Double fee) {
+		this.fee = fee;
+	}
 
 }
