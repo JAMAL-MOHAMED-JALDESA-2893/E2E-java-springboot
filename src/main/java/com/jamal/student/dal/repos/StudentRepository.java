9e0725d8f3445c03dc03ecc25bfb92e2571996aa
Jamal Hassan <jayhamo22@gmail.com>
2022-11-23 12:34:56 +0300

    add CRUD Repo package

---
+++ src/main/java/com/jamal/student/dal/repos/StudentRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jamal.student.dal.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Admin
+ *
+ */
+public interface StudentRepository extends CrudRepository<T, ID> {
+
+}
