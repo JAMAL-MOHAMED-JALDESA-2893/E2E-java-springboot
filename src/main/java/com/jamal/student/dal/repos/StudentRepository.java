00ff170d20366a55284791c8a709957fa6f991f4
Jamal Hassan <jayhamo22@gmail.com>
2022-11-23 12:38:41 +0300

    pointed the table in Repo interface

--- src/main/java/com/jamal/student/dal/repos/StudentRepository.java
+++ src/main/java/com/jamal/student/dal/repos/StudentRepository.java
@@ -5,10 +5,12 @@ package com.jamal.student.dal.repos;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.jamal.student.dal.entities.Student;
+
 /**
  * @author Admin
  *
  */
-public interface StudentRepository extends CrudRepository<T, ID> {
+public interface StudentRepository extends CrudRepository<Student, Long> {
 
 }
