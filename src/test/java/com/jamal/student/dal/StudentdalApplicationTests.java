4e9a6324ee830fe3261ffd70e0e10ca9ec3f4f6f
Jamal Hassan <jayhamo22@gmail.com>
2022-11-23 14:11:24 +0300

    creating tests

--- src/test/java/com/jamal/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/jamal/student/dal/StudentdalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentdalApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public void testCreateStudent() {
+		
 	}
 
 }
