ab3e8b84fcecf4a71f5b9608225a5afd4191367d
Jamal Hassan <jayhamo22@gmail.com>
2022-11-24 11:06:17 +0300

    add mapping to the database

--- src/main/java/com/jamal/student/dal/entities/Student.java
+++ src/main/java/com/jamal/student/dal/entities/Student.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class Student {
     
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	@Column(name="sname")
 	private String name;
--- src/test/java/com/jamal/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/jamal/student/dal/StudentdalApplicationTests.java
@@ -1,13 +1,28 @@
 package com.jamal.student.dal;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jamal.student.dal.entities.Student;
+import com.jamal.student.dal.repos.StudentRepository;
+
+
 
 @SpringBootTest
 class StudentdalApplicationTests {
-
+	
+	@Autowired
+	private StudentRepository studentRepository;
+    
+	@Test
 	public void testCreateStudent() {
+		Student student = new Student();
+		student.setName("Jamal");
+		student.setCourse("Java Web Services");
+		student.setFee(30d);
 		
+		studentRepository.save(student);
 	}
 
 }
